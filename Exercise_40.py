b31db04eb6fcc4e83d1ca1d68af2312c4ed1f190
Jeff <pctech5510@gmail.com>
2024-11-28 16:45:13 -0500

    Added Comment to tell what the program does

--- Exercise_40.py
+++ Exercise_40.py
@@ -1,3 +1,4 @@
+# Ask the user what year they were born, and tell them their age
 birth_year = input('What year were you born? ')
 birth_year =  2024 - int(birth_year)
 print(f"Your age is {birth_year}")
