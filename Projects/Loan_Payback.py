e60bb1f2eaa3d8a62f69c3154a236832221eeef1
Jeff <pctech5510@gmail.com>
2024-12-06 18:43:02 -0500

    Create Loan_Payback.py

---
+++ Projects/Loan_Payback.py
@@ -0,0 +1,11 @@
+''' Create a loan calculator to tell the user how much
+interest they will pay if they pay the loan back on time,
+and if they pay it off early.'''
+
+loan_Amount = print(input("How much money did you borrow?: "))
+interest= print(input("What is the interest rate?: "))
+term = print(input("What is the term of the loan?: "))
+monthly_Payment = print(input("What is the monthly payment?: "))
+
+
+
